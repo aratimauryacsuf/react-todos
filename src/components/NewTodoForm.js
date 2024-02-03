bbf1d1be1aeba3611413c5f6edea265faec39b7f
aratim <aratimaurya@csu.fullerton.edu>
2024-02-03 00:34:04 -0800

    Dynamic form change input

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 function NewTodoForm(){
+
+    const [description, setDescription]= useState('');
+    const [assigned, setAssigned] = useState('');
+
+    // const descriptionChange = (event) =>{
+    //     console.log('description', event.target.value);
+    //     setDescription(event.target.value);
+    // }
+    // const assignedChange = (event) =>{
+    //     console.log('assigned',event.target.value);
+    //     setAssigned(event.target.value);
+    // }
     return(
         <div className='mt-5'>
             <form>
                 <div className='mb-3'>
-                    <label className='form-label'>Assigned</label>
-                    <input type='text' className='form-contol' required></input>
+                    <label className='form-label'>Assigned </label><br/>
+                    <input 
+                    type='text' 
+                    className='form-contol'
+                     required
+                     onChange={e => setAssigned(e.target.value)}
+                     value={assigned}></input>
                 </div>
                 <div className='mb-3'>
-                    <label className='form-label'>Description</label>
-                    <textarea type='text' className='form-contol' rows={3} required></textarea>
+                    <label className='form-label'>Description </label><br/> 
+                    <textarea
+                    className='form-contol' 
+                    rows={3} 
+                    required
+                    onChange={e => setDescription(e.target.value)}
+                    value={description}></textarea>
                 </div>
                 <button type='button' className='bt btn-primary mt-3'>Add Todo</button>
             </form>
