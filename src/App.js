335ea6de6d0c493ba1864a823f8e648d07be090d
aratim <aratimaurya@csu.fullerton.edu>
2024-02-02 23:37:32 -0800

    data handling react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 
+import React,{useState} from 'react';
 import './App.css';
 
 import TodoTable from './components/TodoTable';
 
 function App() {
-const todos = [
+const [todos, setTodos] = useState([
   {rowNum: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
   {rowNum: 2, rowDescription: 'Water Plants', rowAssigned: 'User Two'},
   {rowNum: 3, rowDescription: 'Make Dinner', rowAssigned: 'User One'},
   {rowNum: 4, rowDescription: 'Charge Phone', rowAssigned: 'User One'}
-]
+])
 
 const addTodo=() => {
   console.log('Our addTodo btn has been clicked!');
   if(todos.length > 0){
     const newTodo = {
       rowNum: todos.length+1,rowDescription: 'New Todo', rowAssigned:'User Three'
-    }
-    todos.push(newTodo);  
+    };
+    setTodos(todos => [...todos, newTodo])
+    console.log(todos);
   }
-  console.log(todos);
+  
 }
   return (
     <div className= 'mt-5 container'>
